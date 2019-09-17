0cb4a5573cfe4fecf94eaa52dddd5ab02683425f
yuki-inaho <yoshikawa@inaho.co>
2019-09-17 09:16:58 +0900

    Modify initialization

--- aslam_cv/aslam_cameras/include/aslam/cameras/implementation/PinholeProjection.hpp
+++ aslam_cv/aslam_cameras/include/aslam/cameras/implementation/PinholeProjection.hpp
@@ -778,8 +778,16 @@ bool PinholeProjection<DISTORTION_T>::initializeIntrinsics(const std::vector<Gri
   }
 
   //get the median of the guesses
+  /*
   if(f_guesses.empty())
     return false;
+  */
+  if(f_guesses.empty()){
+    //return false;
+    f_guesses.push_back(620.0);
+  }  
+
+  
   double f0 = PinholeHelpers::medianOfVectorElements(f_guesses);
 
   //set the estimate
